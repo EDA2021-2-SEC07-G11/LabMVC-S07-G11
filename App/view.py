ffb99a5785739d7c1c4e6ca3b66b8205014c5a11
alejandro-alvarez-jim <a.alvarezj@uniandes.edu.co>
2021-08-13 16:55:10 -0500

    Entrega Final – laboratorio 1

--- App/view.py
+++ App/view.py
@@ -43,6 +43,7 @@ def printMenu():
     print("1- Cargar Libros")
     print("2- Cargar Tags")
     # TODO: Modificaciones para el laboratorio 1.
+    print("3-Cargar Tags de Libros")
     print("0- Salir")
 
 
@@ -58,6 +59,12 @@ def loadTags():
     Carga los Tags
     """
     return controller.loadTags('GoodReads/tags.csv')
+
+def loadBooksTags():
+    """
+    Carga los Tags
+    """
+    return controller.loadBooksTags('GoodReads/Book_tags-small.csv')
 
 
 """
@@ -77,7 +84,10 @@ while True:
         print('Total de tags cargados: ' + str(lt.size(tags)))
 
     # TODO: Modificaciones para el laboratorio 1.
-
+    elif int(inputs[0])== 3:
+        print("Cargando los Tags de los libros.")
+        bookstags=loadBooksTags()
+        print('Total de tags de libros cargados: ' + str(lt.size(bookstags)))
     else:
         sys.exit(0)
 sys.exit(0)
